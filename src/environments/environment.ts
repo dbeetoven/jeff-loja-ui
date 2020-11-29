0cdc824e355c79cad7a7265ce4d8833f267bd314
dbeetoven <beetovenson@gmail.com>
2020-11-28 22:00:57 -0300

    remove firecode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAmnTiL3tG6iG6VGPIVRhWRTiQKTxDrDn8',
-    authDomain: 'jeffloja-dev.firebaseapp.com',
-    databaseURL: 'https://jeffloja-dev.firebaseio.com',
-    projectId: 'jeffloja-dev',
-    storageBucket: 'jeffloja-dev.appspot.com',
-    messagingSenderId: '670521149341',
-    appId: '1:670521149341:web:7291acfde71f4729ffd174',
-  },
+  firebase: {},
 };
