acc76514709db72c4310ac5a4252f7a8ef83996b
dbeetoven <beetovenson@gmail.com>
2020-11-29 02:03:19 -0300

    Adding new features, product list,  shopping cart and new product article design.

--- src/app/pages/shopping-cart/shopping-cart.component.ts
+++ src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalstorageService } from 'src/app/core/services/localstorage.service';
+import { IProduct } from 'src/app/models';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent implements OnInit {
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  total(): number {
+    return this.productService.productsIntoCart$.state.reduce(
+      (sum, prod) => (sum += prod.quantity),
+      0
+    );
   }
 
+  removeProduct(product: IProduct): void {
+    const productsLeft = this.productService.productsIntoCart$.state.filter(
+      ({ id }) => id !== product.id
+    );
+    this.productService.productsIntoCart$.set(productsLeft);
+  }
+  ngOnInit(): void {}
 }
