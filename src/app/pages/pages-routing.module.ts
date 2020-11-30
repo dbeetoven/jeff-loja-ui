d58285b467cea22cf36cda4c3514790c1a8e2e61
dbeetoven <beetovenson@gmail.com>
2020-11-30 18:53:03 -0300

    #adding upload product component and routes fixes.

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./home/home.module').then((hm) => hm.HomeModule),
+      },
+      {
+        path: 'shoppingCart',
+        loadChildren: () =>
+          import('./shopping-cart/shopping-cart.module').then(
+            (shpm) => shpm.ShoppingCartModule
+          ),
+      },
+      {
+        path: 'upload-product',
+        loadChildren: () =>
+          import('./upload-product/upload-product.module').then(
+            (shpm) => shpm.UploadProductModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}
